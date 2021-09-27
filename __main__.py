0846828de2bf4f55504d680b1996ada0ebe194cc
Huan Jin <hji236@g.uky.edu>
2021-09-27 16:39:32 -0400

    update files

---
+++ __main__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+MDH command-line interface
+
+Usage:
+    mdh -h | --help     for this screen.
+    mdh --full-help     help documentation on all modes.
+    mdh --version       for the version of MDH.
+    mdh download        for update the metabolic database (eg: KEGG).
+    mdh aromatize       for detecting aromatic substructures in the compound.
+    mdh initialize      for constructing compound and reaction entities.
+    mdh harmonize       for
+
+
+"""
+import sys
+
+def main():
+
+

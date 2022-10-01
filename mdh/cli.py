276602e36393c02fbe02ace5d173698d1588aefd
Huan Jin <huanjin@Huans-MacBook-Air.local>
2022-09-30 21:52:53 -0400

    debug

--- mdh/cli.py
+++ mdh/cli.py
@@ -384,7 +384,7 @@ def cli(args):
 
         # kegg_compound = kegg_compound_parsed["cpd:C04618"]
         # metacyc_compound = metacyc_compound_parsed["CPD-13230"]
-        relationship, mapping = metacyc_compound.with_r_pair_relationship(kegg_compound)
+        relationship, mapping = kegg_compound.with_r_pair_relationship(metacyc_compound)
         print(relationship, mapping)
 
 

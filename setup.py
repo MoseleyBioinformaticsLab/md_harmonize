2a075ddbe6c44f54a3c219adae98ac2ee0c12c2a
Huan Jin <hji236@g.uky.edu>
2022-11-22 15:02:56 -0500

    fix doc

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ EXTENSIONS = [
 setup(
         name='MDH',
         version=find_version(),
-        packages=find_packages(),
+        packages=find_packages(exclude=['doc', 'docs', 'vignettes']),
         author='Huan Jin, Hunter N.B. Moseley',
         author_email='jinhuan0905@gmail.com, hunter.moseley@gmail.com',
         description='Methods for harmonizing public metabolic databases',
@@ -55,8 +55,10 @@ setup(
             'Intended Audience :: Science/Research',
             'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
-            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
             ],
 )

5e8291b336600e6cc93d55f0e1f2adfdc6750058
Huan Jin <huanjin@Huans-MacBook-Air.local>
2022-11-22 19:52:56 -0500

    timeout test

--- mdh/KEGG_parser.py
+++ mdh/KEGG_parser.py
@@ -804,19 +804,19 @@ def create_atom_mappings(rclass_directory: str, compounds: dict) -> dict:
             for token in tokens:
                 one_compound_name, the_other_compound_name = token.split("_")
                 if one_compound_name in compounds and the_other_compound_name in compounds:
-                    # start_time = datetime.now()
-                    # results.append(multiple_compound_pair_mappings(rclass_name, rclass_definitions, compounds[one_compound_name], compounds[the_other_compound_name]))
-                    # end_time = datetime.now()
-                    # consumed = end_time - start_time
-                    # print("parsing of this {0} cost {1}".format(rclass_name, consumed.total_seconds()))
-                    compound_pairs.append((compounds[one_compound_name], compounds[the_other_compound_name]))
-        if len(compound_pairs) > 1:
-            with multiprocessing.Pool() as pool:
-                results = pool.starmap(multiple_compound_pair_mappings, ((rclass_name, rclass_definitions, one_compound,
-                                                                 the_other_compound) for one_compound, the_other_compound in
-                                                                compound_pairs))
-        elif len(compound_pairs) == 1:
-            results = [multiple_compound_pair_mappings(rclass_name, rclass_definitions, compound_pairs[0][0], compound_pairs[0][1])]
+                    start_time = datetime.now()
+                    results.append(multiple_compound_pair_mappings(rclass_name, rclass_definitions, compounds[one_compound_name], compounds[the_other_compound_name]))
+                    end_time = datetime.now()
+                    consumed = end_time - start_time
+                    print("parsing of this {0} cost {1}".format(rclass_name, consumed.total_seconds()))
+                    # compound_pairs.append((compounds[one_compound_name], compounds[the_other_compound_name]))
+        # if len(compound_pairs) > 1:
+        #     with multiprocessing.Pool() as pool:
+        #         results = pool.starmap(multiple_compound_pair_mappings, ((rclass_name, rclass_definitions, one_compound,
+        #                                                          the_other_compound) for one_compound, the_other_compound in
+        #                                                         compound_pairs))
+        # elif len(compound_pairs) == 1:
+        #     results = [multiple_compound_pair_mappings(rclass_name, rclass_definitions, compound_pairs[0][0], compound_pairs[0][1])]
 
         for name, mapping in results:
             atom_mappings[rclass_name + "_" + name] = mapping

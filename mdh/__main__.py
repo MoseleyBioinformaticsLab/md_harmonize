53f95e1e22303eaef469a1044b5b6450fd27b9cb
Huan Jin <huanjin@Huans-MacBook-Air.local>
2022-12-03 19:10:54 -0500

    update doc

--- mdh/__main__.py
+++ mdh/__main__.py
@@ -4,12 +4,18 @@ import docopt
 from . import cli
 from . import __version__
 
+
+def main():
+
+    cli.cli(args)
+
+
 if __name__ == "__main__":
 
     args = docopt.docopt(cli.__doc__, version=__version__)
     import cProfile
     profiler = cProfile.Profile()
     profiler.enable()
-    cli.cli(args)
+    main()
     profiler.disable()
     profiler.print_stats()
